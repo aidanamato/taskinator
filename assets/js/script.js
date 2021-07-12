5cb1a7a178eb7618d94291633881c4377afd1d9d
Aidan Amato <aidanamato@comcast.net>
2021-07-11 22:32:21 -0400

    implement add event functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 var formEl = document.querySelector("#task-form");
+var taskNameInput = document.querySelector("input[name='task-name']");
+var taskTypeInput = document.querySelector("select[name='task-type']");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    
+    // create list item
     var listItemEl = document.createElement("li");
-    listItemEl.textContent = "Some content";
+
+    // create div to hold task info
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className="task-info";
+    taskInfoEl.innerHTML="<h3 class='task-name'>" + taskNameInput.value + "</h3><span class='task-type'>" + taskTypeInput.value + "</span>";
+
+    // add div to li and add li to tasksToDo ul
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
