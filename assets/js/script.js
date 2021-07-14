40311766cd2a5f7870ea59809ffd7ddcd60d0a1b
Aidan Amato <aidanamato@comcast.net>
2021-07-13 21:42:32 -0400

    tasks persist after refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -162,6 +162,7 @@ var editTask = function(taskSelected) {
 var deleteTask = function(taskSelected) {
     taskSelected.remove();
 
+    var taskId = parseInt(taskSelected.getAttribute("data-task-id"));
     var updatedTaskArr = [];
 
     for (var i=0; i < tasks.length; i++) {
@@ -205,5 +206,46 @@ var saveTasks = function() {
     localStorage.setItem("id-counter", taskIdCounter);
 }
 
+var loadTasks = function() {
+    // get tasks and Id counter from localStorage
+    if (localStorage.getItem("tasks")) {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+    
+    if (localStorage.getItem("id-counter")) {
+        taskIdCounter = parseInt(localStorage.getItem("id-counter"));
+    }
+    
+    // iterate through array to generate task elements on the page
+    for (var i= 0; i < tasks.length; i++) {
+        var listItemEl = document.createElement("li");
+        listItemEl.setAttribute("data-task-id", tasks[i].id);
+
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.className = "task-info";
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+        listItemEl.appendChild(taskInfoEl);
+
+        var taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+
+        if (tasks[i].status === "to do") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        }
+
+        if (tasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+
+        if (tasks[i].status === "completed") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+    }
+}
+
+loadTasks();
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
